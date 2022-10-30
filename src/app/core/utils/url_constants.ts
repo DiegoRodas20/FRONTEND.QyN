b0d762197d9e182924ee0a6878fba50baa90e02b
yamil esteban <yamil152844@gmail.com>
2022-10-30 16:48:26 -0500

    LISTAR PROVEEDOR

--- src/app/core/utils/url_constants.ts
+++ src/app/core/utils/url_constants.ts
@@ -10,3 +10,4 @@ export const PEDIDO_URL         = `${environment.API_URL}order`
 export const VEHICLE_URL        = `${environment.API_URL}vehicle`
 export const DRIVER_URL         = `${environment.API_URL}driver`
 export const TYPEVEHICLE_URL    = `${environment.API_URL}typevehicle`
+export const SUPPLIER_URL       = `${environment.API_URL}supplier`
--- src/app/modules/admin/almacen/gestionar-proveedor/gestionar-proveedor.component.ts
+++ src/app/modules/admin/almacen/gestionar-proveedor/gestionar-proveedor.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ProductoService } from 'src/app/core/services/product.service';
+import { FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
+import { SupplierService } from 'src/app/core/services/supplier.service';
 
 @Component({
     selector: 'app-gestionar-proveedor',
@@ -8,11 +10,39 @@ import { ProductoService } from 'src/app/core/services/product.service';
 
 export class GestionarProveedorComponent implements OnInit {
 
+    lProveedores: any[] = []
+    Mensaje: string
+    filtro = new FormControl();
+    p: number = 1;
+
     constructor(
+        private _proveedorService: SupplierService,
+        private _router: Router,
     ) { }
 
-    ngOnInit() {
-
+    ngOnInit() {    
+        this.getProveedor()
     }
+
+    async getProveedor(){
+        try {
+            const data: any = await this._proveedorService.getProveedores().toPromise()
+            this.Mensaje = data.message
+            this.lProveedores = data.data
+        }
+        catch (error) {
+            console.log("Error: ", error)
+        }
+     }
+
+     registrarProveedor(){
+        this._router.navigate(['/almacen/gestionarproveedores/registrar'])
+     }
+     verProveedor(idProveedor: string){
+        this._router.navigate(['/almacen/gestionarproveedores/ver/' + idProveedor])
+     }
+     actualizarProveedor(idProveedor: string){
+        this._router.navigate(['/almacen/gestionarproveedores/actualizar/' + idProveedor])
+     }
 
 }
