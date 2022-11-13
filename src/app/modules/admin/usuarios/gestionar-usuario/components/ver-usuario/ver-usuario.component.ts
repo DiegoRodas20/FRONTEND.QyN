4647a9d73a75cbde9aed257a5192c3fd2d7728d3
gabsss1 <gventuram2015@gmail.com>
2022-11-13 17:16:07 -0500

    [CUS-gestionarUsuario] Update ver-usuario-component

--- src/app/modules/admin/usuarios/gestionar-usuario/components/ver-usuario/ver-usuario.component.ts
+++ src/app/modules/admin/usuarios/gestionar-usuario/components/ver-usuario/ver-usuario.component.ts
@@ -1,3 +1,4 @@
+import { DatePipe } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
@@ -5,6 +6,7 @@ import { UserService } from 'src/app/core/services/user.service';
 
 
 @Component({
+    providers: [DatePipe],
     selector: 'app-ver-usuario',
     templateUrl: 'ver-usuario.component.html'
 })
@@ -18,7 +20,8 @@ export class VerUsuarioComponent implements OnInit {
         private _router: Router,
         private _route: ActivatedRoute,
         private _formBuilder: FormBuilder,
-        private _usuarioService: UserService
+        private _usuarioService: UserService,
+        private _datePipe: DatePipe,
     ) { }
 
     ngOnInit() {
@@ -36,11 +39,6 @@ export class VerUsuarioComponent implements OnInit {
         return estado ? 'Activo' : 'Inactivo'
     }
 
-    // formatDate(){
-    //     const date = new Date(this.formUsuario.value.bornDate)
-    //     return `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`
-    // }
-
     crearFormUsuario() {
         this.formUsuario = this._formBuilder.group({
             email: [null, []],
@@ -56,6 +54,9 @@ export class VerUsuarioComponent implements OnInit {
         try {
             const data: any = await this._usuarioService.getUsuarioxID(idUsuario).toPromise()
             this.formUsuario.patchValue({...data.data, isActive: this.setEstado(data.data.isActive), firstName: this.setVacio(data.data.firstName), lastName: this.setVacio(data.data.lastName), surName: this.setVacio(data.data.surName)})
+            let date = new Date(data.data['bornDate'])
+            date.setDate(date.getDate() + 1);
+            this.formUsuario.controls['bornDate'].setValue(this._datePipe.transform(date, 'yyyy-MM-dd'))
             console.log(data.data)
         }
         catch (error) {
