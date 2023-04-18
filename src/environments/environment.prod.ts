b3ac0b7a04ae427af468324b6bbeaa63b4cfe221
Renzo Reccio <43458658+RenzoReccio@users.noreply.github.com>
2023-04-18 17:19:55 -0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -9,7 +9,7 @@ export const environment = {
     messagingSenderId: '836872548783',
   },
   production: true,
-  API_URL: "https://web-production-5687.up.railway.app/",
+  API_URL: "https://api-qyn-m5twffxvna-uc.a.run.app/",
   API_MAPBOX: "https://api.mapbox.com/",
   MAPBOX_KEY: 'pk.eyJ1IjoiZGllZ29yb2RhczIwIiwiYSI6ImNsYWt4bXQ2djAwa3UzeG5wazZ5Y3Z2YXMifQ.D3nmdsWkc-50ltnov6SUCw'
 };
